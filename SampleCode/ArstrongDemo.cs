f8fc2ec99903f97b56ec04a75c29abcd5788245a
aniketkalvit <aniketkalvit@gmail.com>
2023-01-17 16:37:59 +0530

    Add project files.

---
+++ SampleCode/ArstrongDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode
+{
+    internal class ArstrongDemo
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5 - i; j++)
+                {
+                    Console.Write("  ");
+                }
+                // use for loop where k is less than equal to (2 * i -1)  
+                for (int k = 1; k <= (2 * i - 1); k++)
+                {
+                    Console.Write("* "); // print the Star  
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ SampleCode/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    int z = 0;
+        //    do
+        //    {
+        //        Console.WriteLine("Enter two numbers");
+        //        int a = Convert.ToInt32(Console.ReadLine());
+        //        int b = Convert.ToInt32(Console.ReadLine());
+        //        Console.WriteLine("1. Add");
+        //        Console.WriteLine("2. Sub");
+        //        Console.WriteLine("3. Multi");
+        //        Console.WriteLine("select your option");
+        //        int op = Convert.ToInt32(Console.ReadLine());
+        //        switch (op)
+        //        {
+        //            case 1:
+        //                Console.WriteLine($"Addition is {a + b}");
+        //                break;
+        //            case 2:
+        //                Console.WriteLine($"Sub is {a - b}");
+        //                break;
+        //            case 3:
+        //                Console.WriteLine($"Multiplication is {a * b}");
+        //                break;
+        //            default:
+        //                Console.WriteLine("Wrong option selection");
+        //                break;
+        //        }
+        //        Console.WriteLine("Select 1 for continue and 0 for exit");
+        //        z = Convert.ToInt32(Console.ReadLine());
+        //    } while (z == 1);
+        //}
+    }
+}
